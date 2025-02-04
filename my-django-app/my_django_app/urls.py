f76a28773b91b59a5c02750f6b6b1dd2a528d42a
juancasanovalopez <juancasanovalopez@gmail.com>
2025-02-04 16:50:29 +0100

    go back :)

--- my-django-app/my_django_app/urls.py
+++ my-django-app/my_django_app/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from app import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
     path('secured/', views.secured_view, name='secured'),
     path('admin/', admin.site.urls),
     path('auth/', include('social_django.urls', namespace='social')),
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
 ]
