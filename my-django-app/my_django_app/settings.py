0507b9e5e74bf84732144c6c7a968bdcc5bb5fc5
juancasanovalopez <juancasanovalopez@gmail.com>
2025-02-04 09:31:15 +0100

    test server added to settings

--- my-django-app/my_django_app/settings.py
+++ my-django-app/my_django_app/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
 # Security settings
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.11', 'localhost', '127.0.0.1']
 
 # Installed apps
 INSTALLED_APPS = [
